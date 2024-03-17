bad0d44e0ced42c27e26d0290b21f6fed3d30227
kelvinrandu <randukelvin@gmail.com>
2024-03-17 17:09:49 +0300

    fix list api

--- src/app/api/list/route.ts
+++ src/app/api/list/route.ts
@@ -1,5 +1,22 @@
+import { db } from "@/lib/db";
+import { NextResponse } from "next/server";
 export const dynamic = 'force-dynamic'; // static by default, unless reading the request
  
-export function GET(request: Request) {
-  return new Response(`Hello from`);
+export async function GET(request: Request) {
+//   return new Response(`Hello from`);
+try {
+    const livestream = await db.livetream.findMany({
+      select: {
+        id: true,
+        title: true,
+        streamer: true,
+
+        createdAt: true,
+      },
+    });
+
+    return  NextResponse.json({ livestream: livestream });
+  } catch (error) {
+    return new Response(`error`);
+  }
 }
